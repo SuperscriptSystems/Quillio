7289816fc4b7d868799ae7adcce8f967d489399b
thesmartypants <alex@shmulevich.com>
2025-08-04 15:46:00 -0400

    Deleted useless files

--- models/answer_checker.py
+++
@@ -1,9 +0,0 @@
-from models.prompt_builders import AnswerPromptBuilder
-
-class AnswerChecker:
-    def __init__(self, model_func):
-        self.ask_ai = model_func
-
-    def check(self, question, answer, options, isopen, language="english"):
-        prompt = AnswerPromptBuilder.build_check_prompt(question, answer, options, isopen, language)
-        return self.ask_ai(prompt).strip()
