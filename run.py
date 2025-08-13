d1f34dc0c9d1b2bd2cd478ba1bf5507b8da010f5
thesmartypants <alex@shmulevich.com>
2025-08-13 14:50:24 -0400

    Switch to Uvicorn ASGI server for Flask app

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from app.configuration import app, db
 from app.models import User, Course, Lesson
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, use_reloader=False, host='0.0.0.0')
-    
+with app.app_context():
+    db.create_all()
