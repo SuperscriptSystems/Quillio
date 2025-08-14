cbd9e44eeeae4e3b159cc66c658a07a47f7d21ec
thesmartypants <alex@shmulevich.com>
2025-08-14 14:04:34 -0400

    Added flask app to run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from app.configuration import app, db
+from flask import Flask
 from app.models import User, Course, Lesson
 
 with app.app_context():
     db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
