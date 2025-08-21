11629bb857393f957c3734ac3d69960d33ebd360
thesmartypants <alex@shmulevich.com>
2025-08-21 16:11:11 -0400

    Added more questions during sign up

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
+    full_name = db.Column(db.String(120), nullable=True)
     preferred_lesson_length = db.Column(db.Integer, nullable=False, default=15)
     language = db.Column(db.String(10), nullable=False, default='english')
 
