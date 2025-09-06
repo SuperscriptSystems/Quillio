d26d433086e9f3df86e3de3dca6e27abcdff6f0c
thesmartypants <alex@shmulevich.com>
2025-09-06 18:06:47 -0400

    Added reset password button [QL-2]

--- run.py
+++ run.py
@@ -2,9 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from app.configuration import app, db
-from flask import Flask
-from app.models import User, Course, Lesson
-
+from app import routes
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
