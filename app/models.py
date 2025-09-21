af764e1ffbfed872205e1e0d04a4a73610425e59
thesmartypants <alex@shmulevich.com>
2025-09-21 15:23:38 -0400

    Just in case, import db [ql-13]

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app.configuration import db, login_manager
-from .db_utils import get_json_type
+from app.db_utils import get_json_type
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.dialects.postgresql import UUID
